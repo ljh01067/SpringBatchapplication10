be9d7367dc2fdc3114ab7fba04c8912e407b56c3
ljh01067 <leejh01067@gmail.com>
2024-10-22 15:58:49 +0900

    initData 기반, JPA 기반, AppConfig 기반

---
+++ db/init.sql
@@ -0,0 +1,4 @@
+# 개발 DB 생성
+  DROP DATABASE IF EXISTS SpringGatch_app_10__dev;
+  CREATE DATABASE SpringGatch_app_10__dev;
+  USE SpringGatch_app_10__dev;
--- src/main/java/com/koreait/exam/springbatch_app_10/SpringBatchApp10Application.java
+++ src/main/java/com/koreait/exam/springbatch_app_10/SpringBatchApp10Application.java
@@ -1,13 +1,24 @@
 package com.koreait.exam.springbatch_app_10;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBatchApp10Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchApp10Application.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
